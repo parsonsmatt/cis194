72e6ee372613584ff56e064ab771d6b6ba0c8a55
Matt Parsons <parsonsmatt@gmail.com>
2014-10-22 21:53:30 -0400

    reverseList

--- hw1/hw1.hs
+++ hw1/hw1.hs
@@ -3,9 +3,13 @@
 toDigits :: Integer -> [Integer]
 toDigits n
 	| n <= 0    = []
-	| n <  10   = [n]
+	| n <= 9    = [n]
 	| otherwise = concat [toDigits(div n 10), [mod n 10]] 
 
 -- toDigitsRev :: Integer -> [Integer]
+-- toDigitsRev n
 
-
+reverseList :: [a] -> [a]
+reverseList [] = []
+reverseList [x] = [x]
+reverseList (x:xs) = concat [reverseList(xs), [x]]
