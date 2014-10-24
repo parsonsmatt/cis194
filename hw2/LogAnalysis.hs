ebe8617fb4df4feb7db5cb2d936c1b315130febc
Matt Parsons <parsonsmatt@gmail.com>
2014-10-23 22:11:09 -0400

    much parse

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -7,5 +7,38 @@ import Log
 
 -- Exercise #1:
 
+parse :: String -> [LogMessage]
+parse str = map (parseMessage) (lines(str))
+	
+
 parseMessage :: String -> LogMessage
+parseMessage str = LogMessage (getMessageType str)
+								              (getTimeStamp str)
+									            (getString str)
+
+getMessageType :: String -> MessageType
+getMessageType str
+	| head (words str) == "I" = Info
+	| head (words str) == "W" = Warning
+	| head (words str) == "E" = Error (getErrorLevel str) 
+
+getTimeStamp :: String-> TimeStamp
+getTimeStamp str
+	| isError (getMessageType str) = read (head (drop 2 (words(str)))) :: Int
+	| otherwise = read (head (drop 1 (words (str)))) :: Int
+
+getString :: String -> String
+getString str
+	| isError(getMessageType str) = "TODO: Unstub error."
+	| otherwise = "TODO: Unstub I/W"
+
+isError :: MessageType -> Bool
+isError msg
+	| msg == Info = False
+	| msg == Warning = False
+	| head (words (show msg)) == "Error" = True
+  | otherwise = False
+
+getErrorLevel :: String -> Int
+getErrorLevel str = read (head (drop 1 (words str)))
 
