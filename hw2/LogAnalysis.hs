376b1b805cbeeed07bd6d9d8167ff1f135f7cddf
Matt Parsons <parsonsmatt@gmail.com>
2014-10-26 00:40:12 -0400

    formatting

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -13,9 +13,9 @@ parse str = map (parseMessage) (lines(str))
 
 parseMessage :: String -> LogMessage
 parseMessage str = LogMessage msgType ts rest
-						where msgType = getMessageType str
-						      ts      = getTimeStamp str
-						      rest    = getRest str
+                   where msgType = getMessageType str
+                         ts      = getTimeStamp str
+                         rest    = getRest str
 
 getMessageType :: String -> MessageType
 getMessageType str
@@ -36,7 +36,7 @@ getRest str
 
 isError :: MessageType -> Bool
 isError (Error _) = True
-isError _ = False
+isError _         = False
 
 getErrorLevel :: String -> Int
 getErrorLevel = read . head . (drop 1) . words
@@ -45,7 +45,7 @@ getErrorLevel = read . head . (drop 1) . words
 
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) tree = tree
-insert m Leaf = Node Leaf m Leaf
+insert m Leaf           = Node Leaf m Leaf
 insert msg@(LogMessage _ ts _) tree@(Node left currMsg@(LogMessage _ currTs _) right)
 	| ts > currTs = Node left currMsg (insert msg right)
 	| ts < currTs = Node (insert msg left) currMsg right
@@ -54,17 +54,17 @@ insert msg@(LogMessage _ ts _) tree@(Node left currMsg@(LogMessage _ currTs _) r
 -- Exercise #3:
 
 build :: [LogMessage] -> MessageTree
-build [] = Leaf
-build [msg] = insert msg Leaf
+build []     = Leaf
+build [msg]  = insert msg Leaf
 build (m:ms) = insert m (build ms) 
 
 -- Exercise #4:
 
 inOrder :: MessageTree -> [LogMessage]
 inOrder Leaf = []
-inOrder (Node Leaf m Leaf) = [m]
-inOrder (Node Leaf m rtree) = [m] ++ inOrder rtree
-inOrder (Node ltree m Leaf) = inOrder ltree ++ [m]
+inOrder (Node Leaf m Leaf)   = [m]
+inOrder (Node Leaf m rtree)  = [m] ++ inOrder rtree
+inOrder (Node ltree m Leaf)  = inOrder ltree ++ [m]
 inOrder (Node ltree m rtree) = inOrder ltree ++ [m] ++ inOrder rtree
 
 -- Exercise #5:
