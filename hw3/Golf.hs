af97d2749cff158317f3f3e17540a4a9c3390535
Matt Parsons <parsonsmatt@gmail.com>
2014-10-26 13:41:49 -0400

    Initial commit

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,1 +1,11 @@
 -- Homework 3
+
+-- Exercise 1:
+
+skips :: [a] -> [[a]]
+skips [] = []
+
+-- Taken from @Nefrubyr on StackOverflow.com
+every n xs = case drop (n-1) xs of
+             (y:ys) -> y : every n ys
+			 [] -> []
