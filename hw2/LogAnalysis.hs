0ad4eeee2d8751477e429e4b1254476d7926558d
Matt Parsons <parsonsmatt@gmail.com>
2014-10-25 22:23:44 -0400

    Refactored, working.

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -12,33 +12,33 @@ parse str = map (parseMessage) (lines(str))
 	
 
 parseMessage :: String -> LogMessage
-parseMessage str = LogMessage (getMessageType str)
-                              (getTimeStamp str)
-                              (getString str)
+parseMessage str = LogMessage msgType ts rest
+						where msgType = getMessageType str
+						      ts      = getTimeStamp str
+						      rest    = getRest str
 
 getMessageType :: String -> MessageType
 getMessageType str
-	| head (words str) == "I" = Info
-	| head (words str) == "W" = Warning
-	| head (words str) == "E" = Error (getErrorLevel str) 
+	| firstWord == "I" = Info
+	| firstWord == "W" = Warning
+	| firstWord == "E" = Error (getErrorLevel str) 
+	where firstWord = head (words str)
 
-getTimeStamp :: String-> TimeStamp
+getTimeStamp :: String -> TimeStamp
 getTimeStamp str
 	| isError (getMessageType str) = read (head (drop 2 (words(str)))) :: Int
 	| otherwise = read (head (drop 1 (words (str)))) :: Int
 
-getString :: String -> String
-getString str
-	| isError(getMessageType str) = "TODO: Unstub error."
-	| otherwise = "TODO: Unstub I/W"
+getRest :: String -> String
+getRest str  
+	| isError (getMessageType str) = head (drop 3 (words str))
+	| otherwise                    = head (drop 2 (words str))
 
 isError :: MessageType -> Bool
-isError msg
-	| msg == Info = False
-	| msg == Warning = False
-	| head (words (show msg)) == "Error" = True
-	| otherwise = False
+isError (Error _) = True
+isError _ = False
 
 getErrorLevel :: String -> Int
-getErrorLevel str = read (head (drop 1 (words str)))
+getErrorLevel = read . head . (drop 1) . words
 
+-- Exercise #2: 
