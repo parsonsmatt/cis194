07dfd3c5164d226aad8aa5ab695ff7a9d45c055e
Matt Parsons <parsonsmatt@gmail.com>
2014-11-20 22:57:07 -0500

    Stub exercises

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,1 +1,15 @@
 -- Homework 3
+
+module Golf where 
+
+-- Exercise 1: Hopscotch
+
+skips :: [a] -> [[a]]
+
+-- Exercise 2: Local maxima
+
+localMaxima :: [Integer] -> [Integer]
+
+-- Exercise 3: Histogram
+
+histogram :: [Integer] -> String
