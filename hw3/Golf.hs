ece56bc76e38cee79dd5fd50a30778525801bfe5
Matt Parsons <parsonsmatt@gmail.com>
2014-11-25 00:07:58 -0500

    histogram base functions

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,6 +1,8 @@
 -- Homework 3
 
 module Golf where 
+import Data.List
+
 
 -- Exercise 1: Hopscotch
 skips :: [a] -> [[a]]
@@ -44,3 +46,21 @@ localMaxima (x:xs) = if (x > head xs)
 
 histogram :: [Integer] -> String
 histogram [] = ""
+
+-- Construct the list of numbers and occurences
+completeListPairs xs = sort $ union (missingNum xs) (numsAndOccurences xs)
+
+-- A list of missing numbers, paired with 0.
+missingNum xs = zip ([0..9] \\ xs) (repeat 0)
+
+-- Pairs of numbers in list and their occurences.
+numsAndOccurences :: [Integer] -> [(Integer, Integer)]
+numsAndOccurences xs = zip (numbersInList xs) (occurences xs)
+
+-- Gives the numbers in the sorted list.
+numbersInList :: [Integer] -> [Integer]
+numbersInList xs = nub $ sort xs
+
+-- Gives the number of occurences for each number in the sorted list.
+occurences :: [Integer] -> [Integer]
+occurences xs = map (fromIntegral . length) $ group $ sort xs
