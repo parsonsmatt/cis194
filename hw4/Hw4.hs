9a1a8718e0b41e6c9645625d2471df75fa9c898b
Matt Parsons <parsonsmatt@gmail.com>
2015-02-20 23:45:58 -0500

    trying it again

--- hw4/Hw4.hs
+++ hw4/Hw4.hs
@@ -1,24 +1,21 @@
--- Homework 4
-
 module Hw4 where
 
 -- Exercise 1:
 
 fun1 :: [Integer] -> Integer
-fun1 []         = 1
+fun1 []       = 1
 fun1 (x:xs) 
-    | even x    = (x - 2) * fun1 xs
-    | otherwise = fun1 xs
+  | even x    = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
 
 fun1' :: [Integer] -> Integer
 fun1' =  product . filter even . map (subtract 2)
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
-fun2 n | even n    = n + fun2 (n `div` 2)
-       | otherwise = fun2 (3 * n + 1)
+fun2 n 
+  | even n    = n + fun2 (n `div` 2)
+  | otherwise =     fun2 (3 * n + 1)
 
 fun2' :: Integer -> Integer
-fun2' 1 = 0
-fun2' n = 0  
-
+fun2' n = 1
