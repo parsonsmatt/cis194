3ee13de137a653c099bc6255cbbab03b01cc27f2
Matt Parsons <parsonsmatt@gmail.com>
2015-02-26 13:29:45 -0500

    dat typeclass

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -12,3 +12,17 @@ eval (Mul a b) = eval a * eval b
 -- Exercise #2:
 evalStr :: String -> Maybe Integer
 evalStr str = eval <$> parseExp Lit Add Mul str
+
+-- Exercise #3:
+class Expr a where
+    lit :: Integer -> a
+    add :: a -> a -> a
+    mul :: a -> a -> a
+
+instance Expr ExprT where
+    lit a   = Lit a
+    add a b = Add a b
+    mul a b = Mul a b
+
+-- Exercise #4:
+
