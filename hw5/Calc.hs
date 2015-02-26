9e8041d31600b6112997b85a32116416488c5280
Matt Parsons <parsonsmatt@gmail.com>
2015-02-26 12:12:34 -0500

    add signature for parseexpr

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -1,7 +1,12 @@
 module Calc where
 import ExprT
+import Parser
 
+-- Exercise #1:
 eval :: ExprT -> Integer
 eval (Lit a) = a
 eval (Add a b) = eval a + eval b
 eval (Mul a b) = eval a * eval b
+
+-- Exercise #2:
+-- evalStr :: String -> Maybe Integer
