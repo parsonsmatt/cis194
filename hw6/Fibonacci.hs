8ca3fde4478f64734d83884145a84eb2c4d5d301
Matt Parsons <parsonsmatt@gmail.com>
2015-02-28 14:48:55 -0500

    zipStreams

--- hw6/Fibonacci.hs
+++ hw6/Fibonacci.hs
@@ -16,7 +16,7 @@ fibs1 = map fib [1..]
 -- Exercise #2:
 
 fibs2 :: [Integer]
-fibs2 = 0 : 1 : zipWith (+) fibs2 tail fibs2
+fibs2 = 0 : 1 : zipWith (+) (fibs2) (tail fibs2)
 
 -- Exercise #3:
 
@@ -47,8 +47,16 @@ streamFromSeed f a = Elem a (streamFromSeed f (f a))
 nats :: Stream Integer
 nats = streamFromSeed (+1) 0
 
+evens :: Stream Integer
+evens = streamFromSeed (+2) 0
+
 -- nth element in the stream (assuming first element n=1) is the largest power
--- of 2 which evenly divides n
+-- of 2 which evenly divides n: n=8 => 3 (2^3), n=6 => 1 (2^1)
 ruler :: Stream Integer
+ruler = nats
 
 interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Elem a as) (Elem b bs) = Elem a (Elem b (interleaveStreams as bs))
+
+zipStreams :: Stream a -> Stream b -> Stream (a,b)
+zipStreams (Elem a as) (Elem b bs) = Elem (a,b) (zipStreams as bs)
