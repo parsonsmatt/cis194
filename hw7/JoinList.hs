ada3b4baf754331230804649eb88d0316b52e516
Matt Parsons <parsonsmatt@gmail.com>
2015-03-03 11:18:42 -0500

    implement instance Buffer

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -10,12 +10,12 @@ data JoinList m a = Empty
                   | Append m (JoinList m a) (JoinList m a)
     deriving (Eq, Show)
 
-single :: a -> JoinList (Size) a
-single a = Single (Size 1) a
+single :: a -> JoinList Size a
+single = Single (Size 1)
 
 -- Exercise #1:
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
-jl1 +++ jl2 = Append ((tag jl1) <> (tag jl2)) jl1 jl2
+jl1 +++ jl2 = Append (tag jl1 <> tag jl2) jl1 jl2
 
 
 tag :: Monoid m => JoinList m a -> m
@@ -36,38 +36,47 @@ indexJ x (Append m l r)
     | x < intFromTag l   = indexJ x l
     | otherwise          = indexJ (x - intFromTag l) r
 
+
 intFromSized :: Sized a => a -> Int
 intFromSized = getSize . size
 
+
 intFromTag :: (Monoid m, Sized m) => JoinList m a -> Int
 intFromTag = intFromSized . tag
+
 
 dropJ :: (Sized b, Monoid b) =>
          Int -> JoinList b a -> JoinList b a
 dropJ _ Empty = Empty
 dropJ x (Single m a) 
     | x > 0     = Empty
-    | otherwise = (Single m a)
+    | otherwise = Single m a
 dropJ x (Append m l r)
     | x > intFromSized m = Empty
-    | x < sl             = (dropJ x l) +++ r
+    | x < sl             = dropJ x l +++ r
     | otherwise          = dropJ (x - sl) r 
-  where sl = intFromTag l
+    where sl = intFromTag l
+
 
 takeJ :: (Sized b, Monoid b) =>
          Int -> JoinList b a -> JoinList b a
 takeJ _ Empty = Empty
 takeJ x (Single m a)
-    | x < 0     = (Single m a)
+    | x < 0     = Single m a
     | otherwise = Empty
 takeJ x (Append m l r)
     | x < 0     = Append m l r
-    | x < sl    = (takeJ x l)
-    | otherwise = l +++ (takeJ (x - sl) r)
-  where sl = intFromTag l
+    | x < sl    = takeJ x l
+    | otherwise = l +++ takeJ (x - sl) r
+    where sl = intFromTag l
+
 
 scoreLine :: String -> JoinList Score String
 scoreLine str = Single (scoreString str) str
+
+
+getSzInt :: JoinList (Score, Size) String -> Int 
+getSzInt = fromIntegral . getSize . snd . tag
 
 
 instance Buffer (JoinList (Score, Size) String) where
@@ -84,7 +93,26 @@ instance Buffer (JoinList (Score, Size) String) where
     line x (Single _ a) | x == 0    = Just a
                       | otherwise = Nothing
     line x (Append (sc, sz) l r)
-        | x > getSize sz      = Nothing
-        | x < getSz l = line x l
-        | otherwise   = line (x - getSz l) r
-      where getSz = fromIntegral . getSize . snd . tag
+        | x > getSize sz = Nothing
+        | x < szl = line x l
+        | otherwise      = line (x - szl) r
+        where szl = getSzInt l
+
+    replaceLine x str b
+        | x < 0          = b
+        | x > numLines b = b
+        | otherwise      = go x str b
+        where go _ _ Empty = Empty
+              go _ str (Single m a) = Single m str 
+              go x str (Append (_, sz) l r)
+                  | x < szl   = go x str l +++ r
+                  | otherwise = l +++ go (x - szl) str r
+                  where szl = getSzInt l
+
+    numLines Empty = 0
+    numLines (Single _ _) = 1
+    numLines (Append (_, sz) _ _) = getSize sz
+
+    value Empty = 0
+    value (Single (sc, si) _) = fromIntegral sc
+    value (Append (sc, _) _ _) = fromIntegral sc
